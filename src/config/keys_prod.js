7227896dc019543421b9ea128c85b36e88308737
Tim Vanloocke <tim.vanloocke@roeselare.be>
2020-06-16 11:19:02 +0200

    backend url hardcoded

--- src/config/keys_prod.js
+++ src/config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BACKEND_URI: process.env.BACKEND_URI,
+  BACKEND_URI: "https://samentuin-backend.herokuapp.com/",
   secretOrKey: process.env.SECRET_OR_KEY,
 };
--- src/redux/actions/zaaikalenderActions.js
+++ src/redux/actions/zaaikalenderActions.js
@@ -2,6 +2,6 @@ import axios from "axios";
 const backendURI = require("../../config/keys").BACKEND_URI;
 
 export const getZaaikalenderinfo = () => async (dispatch) => {
-  const res = await axios.get(`${backendURI}/api/zaaikalender`);
+  const res = await axios.get(`${backendURI}/api/groenteninfo`);
   dispatch({ type: "GET_ZAAIKALENDER", payload: res.data });
 };
