22e8b93a4af4e5ec19979e5545e1f4a7fc25a9ec
Tim Vanloocke <tim.vanloocke@gmail.com>
2023-05-15 12:39:27 +0200

    backend op netlify

--- src/config/keys_prod.js
+++ src/config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BACKEND_URI: "https://samentuin-backend.herokuapp.com",
+  BACKEND_URI: "https://dapper-druid-b2ac3a.netlify.app",
   secretOrKey: process.env.SECRET_OR_KEY,
 };
