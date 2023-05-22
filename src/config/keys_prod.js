cdb4663a54ec864833c719f09ffff856770bb369
Tim Vanloocke <tim.vanloocke@gmail.com>
2023-05-22 10:05:30 +0200

    heroku vives

--- src/config/keys_prod.js
+++ src/config/keys_prod.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BACKEND_URI: "https://dapper-druid-b2ac3a.netlify.app",
+  BACKEND_URI: "https://samentuin-backend-vives.herokuapp.com/",
   secretOrKey: process.env.SECRET_OR_KEY,
 };
